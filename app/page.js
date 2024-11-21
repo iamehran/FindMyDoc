909bd04260afbabcadbff8bb03e007975ff425a0
Mehran <workararsi@gmail.com>
2024-11-21 22:29:32 +0530

    prod issue

--- app/page.js
+++ app/page.js
@@ -1,19 +1,21 @@
 'use client';
 
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { TokenManager } from '../lib/token-manager';
 import DocumentList from '../components/DocumentList';
 
 function TokenHandler() {
   const searchParams = useSearchParams();
-  const tokens = searchParams.get('tokens');
   
-  if (tokens) {
-    TokenManager.setTokens(JSON.parse(decodeURIComponent(tokens)));
-    window.history.replaceState({}, '', '/');
-  }
-  
+  useEffect(() => {
+    const tokens = searchParams.get('tokens');
+    if (tokens) {
+      TokenManager.setTokens(JSON.parse(decodeURIComponent(tokens)));
+      window.history.replaceState({}, '', '/');
+    }
+  }, [searchParams]);
+
   return null;
 }
 
